995faa6c590e37c885dc2ba5db5b98fd07536843
Hrithik Naha <naha.hrithkk@gmail.com>
2023-07-20 18:33:26 +0530

    add default img logic

--- src/components/CastList.js
+++ src/components/CastList.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+const defaultImg = "https://t4.ftcdn.net/jpg/02/17/34/67/240_F_217346782_7XpCTt8bLNJqvVAaDZJwvZjm0epQmj6j.jpg";
+
 const CastList = ({ casts }) => {
     return (
         <div className="mt-2 mb-8">
@@ -11,6 +13,7 @@ const CastList = ({ casts }) => {
                             src={`https://image.tmdb.org/t/p/w300/${actor.profile_path}`}
                             alt={actor.name}
                             className="rounded-t w-full"
+                            onError={(e) => (e.target.src = defaultImg)}
                         />
                         <div className="p-3">
                             <h3 className="text-sm font-semibold">{actor.name}</h3>
--- src/components/CrewList.js
+++ src/components/CrewList.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+const defaultImg = "https://t4.ftcdn.net/jpg/02/17/34/67/240_F_217346782_7XpCTt8bLNJqvVAaDZJwvZjm0epQmj6j.jpg";
+
 const CrewList = ({ crews }) => {
     return (
         <div className="my-8">
@@ -11,6 +13,7 @@ const CrewList = ({ crews }) => {
                             src={`https://image.tmdb.org/t/p/w300/${actor.profile_path}`}
                             alt={actor.name}
                             className="rounded-t w-full"
+                            onError={(e) => (e.target.src = defaultImg)}
                         />
                         <div className="p-3">
                             <h3 className="text-sm font-semibold">{actor.name}</h3>
