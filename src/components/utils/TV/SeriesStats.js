7b39cb566815bec593efbc4db6e467be41839a80
Hrithik Naha <naha.hrithkk@gmail.com>
2023-07-27 19:52:25 +0530

    changed statsitics look and feel

--- src/components/utils/TV/SeriesStats.js
+++ src/components/utils/TV/SeriesStats.js
@@ -5,8 +5,8 @@ const COLORS = ["#172554", "#1e3a8a", "#1e40af", "#1d4ed8", "#2563eb", "#3b82f6"
 
 const SeriesStats = ({ series, selected }) => {
     return (
-        <div>
-            <div className="flex flex-wrap gap-6 mt-2">
+        <div className="mt-8">
+            <div className="flex flex-wrap gap-6 mt-2 justify-center">
                 <div className="bg-white shadow-md rounded-lg p-6 w-60">
                     <h2 className="text-xl font-semibold mb-4">{selected} Runtime</h2>
                     <p className="text-3xl font-bold text-blue-500">{series.totalWatchedRuntime} hrs</p>
@@ -27,54 +27,54 @@ const SeriesStats = ({ series, selected }) => {
             </div>
             <div className="flex gap-4 mt-8 justify-between">
                 <div className="flex flex-col justify-center items-center">
-                    <PieChart width={250} height={250}>
-                        <Pie dataKey="count" data={series.genreSeriesDataset} innerRadius={80} outerRadius={100}>
+                    <PieChart width={400} height={400}>
+                        <Pie dataKey="count" data={series.genreSeriesDataset} innerRadius={60} outerRadius={100}>
                             {series.genreSeriesDataset.map((entry, index) => (
                                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                             ))}
                         </Pie>
                         <Tooltip />
+                        <Legend />
                     </PieChart>
-                    <div className="text-2xl">Genres</div>
                 </div>
                 <div className="flex flex-col items-center">
-                    <PieChart width={250} height={250}>
-                        <Pie dataKey="count" data={series.statusSeriesDataset} innerRadius={80} outerRadius={100}>
+                    <PieChart width={400} height={400}>
+                        <Pie dataKey="count" data={series.statusSeriesDataset} innerRadius={60} outerRadius={100}>
                             {series.statusSeriesDataset.map((entry, index) => (
                                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                             ))}
                         </Pie>
                         <Tooltip />
+                        <Legend />
                     </PieChart>
-                    <div className="text-2xl">Status</div>
                 </div>
                 <div className="flex flex-col items-center">
-                    <PieChart width={250} height={250}>
-                        <Pie dataKey="count" data={series.languageSeriesDataset} innerRadius={80} outerRadius={100}>
+                    <PieChart width={400} height={400}>
+                        <Pie dataKey="count" data={series.languageSeriesDataset} innerRadius={60} outerRadius={100}>
                             {series.languageSeriesDataset.map((entry, index) => (
                                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                             ))}
                         </Pie>
                         <Tooltip />
+                        <Legend />
                     </PieChart>
-                    <div className="text-2xl">Language</div>
                 </div>
             </div>
             <div className="flex flex-col gap-4 mt-16 items-center">
-                <BarChart width={200} height={200} data={series.releaseYearSeriesDataset}>
+                <BarChart width={1000} height={300} data={series.releaseYearSeriesDataset}>
                     <XAxis dataKey="name" />
                     <Tooltip />
                     <Bar dataKey="count" fill="#172554" />
+                    <Legend />
                 </BarChart>
-                <div className="text-2xl">Release Year</div>
             </div>
             <div className="flex gap-4 mt-8 justify-between">
                 <div className="flex flex-col justify-center items-center">
-                    <PieChart width={250} height={250}>
+                    <PieChart width={400} height={400}>
                         <Pie
                             dataKey="count"
                             data={series.originCountrySeriesDataset}
-                            innerRadius={80}
+                            innerRadius={60}
                             outerRadius={100}
                         >
                             {series.originCountrySeriesDataset.map((entry, index) => (
@@ -82,44 +82,15 @@ const SeriesStats = ({ series, selected }) => {
                             ))}
                         </Pie>
                         <Tooltip />
+                        <Legend />
                     </PieChart>
-                    <div className="text-2xl">Origin Country</div>
                 </div>
                 <div className="flex flex-col items-center">
-                    <PieChart width={250} height={250}>
-                        <Pie dataKey="count" data={series.networkSeriesDataset} innerRadius={80} outerRadius={100}>
-                            {series.networkSeriesDataset.map((entry, index) => (
-                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                            ))}
-                        </Pie>
-                        <Tooltip />
-                    </PieChart>
-                    <div className="text-2xl">Network</div>
-                </div>
-            </div>
-            <div className="flex gap-4 mt-8 justify-between">
-                <div className="flex flex-col justify-center items-center">
-                    <PieChart width={250} height={250}>
-                        <Pie
-                            dataKey="count"
-                            data={series.productionCompaniesSeriesDataset}
-                            innerRadius={80}
-                            outerRadius={100}
-                        >
-                            {series.productionCompaniesSeriesDataset.map((entry, index) => (
-                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                            ))}
-                        </Pie>
-                        <Tooltip />
-                    </PieChart>
-                    <div className="text-2xl">Productions</div>
-                </div>
-                <div className="flex flex-col items-center">
-                    <PieChart width={250} height={250}>
+                    <PieChart width={400} height={400}>
                         <Pie
                             dataKey="count"
                             data={series.productionCountriesSeriesDataset}
-                            innerRadius={80}
+                            innerRadius={60}
                             outerRadius={100}
                         >
                             {series.productionCountriesSeriesDataset.map((entry, index) => (
@@ -127,8 +98,79 @@ const SeriesStats = ({ series, selected }) => {
                             ))}
                         </Pie>
                         <Tooltip />
+                        <Legend />
                     </PieChart>
-                    <div className="text-2xl">Country Productions</div>
+                </div>
+            </div>
+
+            <div className="flex justify-between items-start mt-8">
+                <div class="flex flex-col my-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-blue-800">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        Production Company
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        Watches
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                {series.productionCompaniesSeriesDataset.map((e) => (
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{e.name}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{e.count}</div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="flex flex-col my-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-blue-800">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        Network
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        Watches
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                {series.networkSeriesDataset.map((e) => (
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{e.name}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{e.count}</div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/utils/movie/MovieStats.js
+++ src/components/utils/movie/MovieStats.js
@@ -5,8 +5,8 @@ const COLORS = ["#172554", "#1e3a8a", "#1e40af", "#1d4ed8", "#2563eb", "#3b82f6"
 
 const MovieStats = ({ movies, selected }) => {
     return (
-        <div>
-            <div className="flex flex-wrap gap-6 mt-2">
+        <div className="mt-8">
+            <div className="flex flex-wrap gap-6 mt-2 justify-center">
                 <div className="bg-white shadow-md rounded-lg p-6 w-60">
                     <h2 className="text-xl font-semibold mb-4">{selected} Runtime</h2>
                     <p className="text-3xl font-bold text-blue-500">{movies.totalRuntimeMovie} hrs</p>
@@ -20,85 +20,35 @@ const MovieStats = ({ movies, selected }) => {
                     <p className="text-3xl font-bold text-blue-500">{movies.totalMovies}</p>
                 </div>
             </div>
-            <div className="flex gap-4 mt-8 justify-between">
+            <div className="flex gap-4 my-8 justify-between">
                 <div className="flex flex-col justify-center items-center">
-                    <PieChart width={250} height={250}>
-                        <Pie dataKey="count" data={movies.genreMovieDataset} innerRadius={80} outerRadius={100}>
+                    <PieChart width={400} height={400}>
+                        <Pie dataKey="count" data={movies.genreMovieDataset} innerRadius={60} outerRadius={100}>
                             {movies.genreMovieDataset.map((entry, index) => (
                                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                             ))}
                         </Pie>
                         <Tooltip />
+                        <Legend />
                     </PieChart>
-                    <div className="text-2xl">Genres</div>
                 </div>
                 <div className="flex flex-col items-center">
-                    <PieChart width={250} height={250}>
-                        <Pie dataKey="count" data={movies.languageMovieDataset} innerRadius={80} outerRadius={100}>
+                    <PieChart width={400} height={400}>
+                        <Pie dataKey="count" data={movies.languageMovieDataset} innerRadius={60} outerRadius={100}>
                             {movies.languageMovieDataset.map((entry, index) => (
                                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                             ))}
                         </Pie>
                         <Tooltip />
+                        <Legend />
                     </PieChart>
-                    <div className="text-2xl">Language</div>
-                </div>
-            </div>
-            <div className="flex flex-col gap-4 mt-16 items-center">
-                <BarChart width={200} height={200} data={movies.releaseYearMovieDataset}>
-                    <XAxis dataKey="name" />
-                    <Tooltip />
-                    <Bar dataKey="count" fill="#172554" />
-                </BarChart>
-                <div className="text-2xl">Release Year</div>
-            </div>
-            <div className="flex gap-4 mt-8 justify-between">
-                <div className="flex flex-col justify-center items-center">
-                    <PieChart width={250} height={250}>
-                        <Pie dataKey="count" data={movies.castMovieDataset} innerRadius={80} outerRadius={100}>
-                            {movies.genreMovieDataset.map((entry, index) => (
-                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                            ))}
-                        </Pie>
-                        <Tooltip />
-                    </PieChart>
-                    <div className="text-2xl">Cast</div>
                 </div>
                 <div className="flex flex-col items-center">
-                    <PieChart width={250} height={250}>
-                        <Pie dataKey="count" data={movies.directorMovieDataset} innerRadius={80} outerRadius={100}>
-                            {movies.languageMovieDataset.map((entry, index) => (
-                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                            ))}
-                        </Pie>
-                        <Tooltip />
-                    </PieChart>
-                    <div className="text-2xl">Directors</div>
-                </div>
-            </div>
-            <div className="flex gap-4 mt-8 justify-between">
-                <div className="flex flex-col justify-center items-center">
-                    <PieChart width={250} height={250}>
-                        <Pie
-                            dataKey="count"
-                            data={movies.productionCompaniesMovieDataset}
-                            innerRadius={80}
-                            outerRadius={100}
-                        >
-                            {movies.productionCompaniesMovieDataset.map((entry, index) => (
-                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                            ))}
-                        </Pie>
-                        <Tooltip />
-                    </PieChart>
-                    <div className="text-2xl">Productions</div>
-                </div>
-                <div className="flex flex-col items-center">
-                    <PieChart width={250} height={250}>
+                    <PieChart width={400} height={400}>
                         <Pie
                             dataKey="count"
                             data={movies.productionCountriesMovieDataset}
-                            innerRadius={80}
+                            innerRadius={60}
                             outerRadius={100}
                         >
                             {movies.productionCountriesMovieDataset.map((entry, index) => (
@@ -106,8 +56,120 @@ const MovieStats = ({ movies, selected }) => {
                             ))}
                         </Pie>
                         <Tooltip />
+                        <Legend />
                     </PieChart>
-                    <div className="text-2xl">Country Productions</div>
+                </div>
+            </div>
+            <div className="flex flex-col gap-4 my-24 items-center">
+                <BarChart width={1000} height={300} data={movies.releaseYearMovieDataset}>
+                    <XAxis dataKey="name" />
+                    <Tooltip />
+                    <Bar dataKey="count" fill="#172554" />
+                    <Legend />
+                </BarChart>
+            </div>
+            <div className="flex justify-between items-start mt-8">
+                <div class="flex flex-col my-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-blue-500">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        Actor
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        Movies
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                {movies.castMovieDataset.map((e) => (
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{e.name}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{e.count}</div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="flex flex-col my-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-blue-500">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        Director
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        Movies
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                {movies.directorMovieDataset.map((e) => (
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{e.name}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{e.count}</div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="flex flex-col my-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-blue-500">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        Production Company
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        Movies
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                {movies.productionCompaniesMovieDataset.map((e) => (
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{e.name}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{e.count}</div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
