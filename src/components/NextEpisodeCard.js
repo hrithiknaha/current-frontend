0ef4b9248024851be253276c1d00e94311b4ffd4
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-04 23:49:08 +0530

    add next episode func

---
+++ src/components/NextEpisodeCard.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const NextEpisodeCard = ({ series, nextEpisode, watchedEpisodes }) => {
+    const totalEpisodes = series.number_of_episodes;
+    const watchedPercentage = (watchedEpisodes.length / totalEpisodes) * 100;
+
+    return (
+        <Link
+            to={`tv/${series.id}/season/${nextEpisode.season_number}/episode/${nextEpisode.episode_number}`}
+            className="bg-white rounded-lg shadow-md p-6 w-64">
+            <h2 className="text-xl font-bold mb-2">{series.name}</h2>
+            <p className="text-gray-600 mb-2">
+                S{nextEpisode.season_number} | E{nextEpisode.episode_number}
+            </p>
+            <div className="h-1 w-full bg-gray-300 mt-4">
+                <div className="h-1 bg-blue-500" style={{ width: `${watchedPercentage}%` }}></div>
+            </div>
+        </Link>
+    );
+};
+
+export default NextEpisodeCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import { axiosPublicInstance } from "../configs/axios";
+import { axiosPublicInstance, axiosPrivateInstance } from "../configs/axios";
 import SmallLoadingSpinner from "../components/configs/SmallLoadingSpinner";
+import NextEpisodeCard from "../components/NextEpisodeCard";
 
 function Home() {
     const [trending, setTrending] = useState();
@@ -10,7 +12,11 @@ function Home() {
 
     const [query, setQuery] = useState("");
 
+    const [nextDetails, setNextDetails] = useState([]);
+    const [isSeriesLoading, setIsSeriesLoading] = useState(true);
+
     const navigate = useNavigate();
+    const auth = useSelector((state) => state.auth);
 
     useEffect(() => {
         axiosPublicInstance.get("/api/tmdb/trending").then(({ data }) => {
@@ -19,10 +25,75 @@ function Home() {
         });
     }, []);
 
+    useEffect(() => {
+        const axiosInstance = axiosPrivateInstance(auth);
+
+        axiosInstance.get(`/api/users/${auth.username}`).then(({ data }) => {
+            if (data.series.length > 0) {
+                const promises = data.series.map((show) => {
+                    const series_id = show.series_id;
+                    let seasonNumber = show.episodes[show.episodes?.length - 1].season_number;
+                    let episodeNumber = show.episodes[show.episodes?.length - 1].episode_number;
+
+                    return axiosPublicInstance
+                        .get(`/api/tmdb/series/${series_id}`)
+                        .then((response) => {
+                            const seriesDetails = response.data;
+
+                            //total seasons
+                            const totalSeasons = seriesDetails.number_of_seasons;
+
+                            //Next Episode?
+                            const currentSeason = seriesDetails.seasons.filter(
+                                (season) => season.season_number === seasonNumber
+                            )[0];
+
+                            if (seasonNumber <= totalSeasons) {
+                                if (episodeNumber < currentSeason.episode_count) episodeNumber += 1;
+                                else if (episodeNumber === currentSeason.episode_count) {
+                                    episodeNumber = 1;
+                                    seasonNumber += 1;
+                                }
+
+                                return axiosPublicInstance
+                                    .get(
+                                        `/api/tmdb/series/${series_id}/season/${seasonNumber}/episode/${episodeNumber}`
+                                    )
+                                    .then((nextEpisodeData) => {
+                                        const nextEpisode = nextEpisodeData.data;
+                                        return { seriesDetails, nextEpisode, show };
+                                    });
+                            } else {
+                                return null; // Return null for cases where there is no next episode
+                            }
+                        })
+                        .catch((error) => {
+                            console.error(error); // Handle errors for individual Axios requests locally
+                            return null; // Return null for failed requests
+                        });
+                });
+
+                Promise.all(promises.filter(Boolean)) // Filter out null values from the promises array
+                    .then((results) => {
+                        const validResult = results
+                            .filter((r) => r !== null)
+                            .sort((a, b) => (a.show.updatedAt < b.show.updatedAt ? 1 : -1));
+                        setNextDetails(validResult);
+                        setIsSeriesLoading(false);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        setIsSeriesLoading(false);
+                    });
+            }
+        });
+    }, [auth.username]);
+
     const handleSubmit = (e) => {
         e.preventDefault();
         navigate(`/search?query=${query}`);
     };
+
     return (
         <div className="min-h-screen  bg-gray-100">
             <div>
@@ -51,6 +122,28 @@ function Home() {
                         </form>
                     </div>
                 </div>
+                {nextDetails && (
+                    <div className="container mx-auto pt-8">
+                        <h1 className="text-2xl">Currently Watching</h1>
+                        <div className="pt-2 flex flex-wrap gap-4">
+                            {isSeriesLoading ? (
+                                <SmallLoadingSpinner />
+                            ) : (
+                                nextDetails.map((data) => {
+                                    return (
+                                        <NextEpisodeCard
+                                            key={data.seriesDetails.id}
+                                            series={data.seriesDetails}
+                                            nextEpisode={data.nextEpisode}
+                                            watchedEpisodes={data.show.episodes}
+                                        />
+                                    );
+                                })
+                            )}
+                        </div>
+                    </div>
+                )}
+
                 <div className="container mx-auto pt-8">
                     <h1 className="text-2xl">Trending this week</h1>
                     {isLoading ? (
