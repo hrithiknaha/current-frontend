6d3a5dec56839d7593e6d715d6234fd3774ef1be
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-14 20:32:19 +0530

    fixed styles

--- src/components/TV/WatchedEpisodeRow.js
+++ src/components/TV/WatchedEpisodeRow.js
@@ -10,15 +10,15 @@ const WatchedEpisodeRow = ({ episode }) => {
             <div key={episode.episode_id} className="bg-white rounded-lg shadow-md p-4 mt-3">
                 <h2 className="text-lg font-semibold mb-2">{episode.name}</h2>
                 <div className="flex items-center justify-between">
-                    <div className="mr-4">
+                    <div className="flex flex-col">
                         <p className="text-gray-600">Episdoe Number:</p>
                         <p className="text-2xl font-semibold">{episode.episode_number}</p>
                     </div>
-                    <div>
+                    <div className="flex flex-col">
                         <p className="text-gray-600">Watch Date:</p>
-                        <p className="text-2xl font-semibold">{moment(episode.date_watched).format("YYYY-MM-DD")}</p>
+                        <p className="text-2xl font-semibold">{moment(episode.date_watched).fromNow()}</p>
                     </div>
-                    <div>
+                    <div className="flex flex-col">
                         <p className="text-gray-600 float-right">Verdict:</p>
                         <p className="text-2xl font-semibold">{getVerdict(episode.rating)}</p>
                     </div>
