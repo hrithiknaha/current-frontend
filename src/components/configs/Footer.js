d110af72f49694871acb5f245778757292c87518
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-19 23:41:46 +0530

    add classname

--- src/components/configs/Footer.js
+++ src/components/configs/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer class="bg-gradient-to-r from-red-500 to-orange-500 text-white py-6">
-            <div class="container mx-auto text-center">
+        <footer className="bg-gradient-to-r from-red-500 to-orange-500 text-white py-6">
+            <div className="container mx-auto text-center">
                 <p>&copy; 2023 Current. All rights reserved.</p>
             </div>
         </footer>
