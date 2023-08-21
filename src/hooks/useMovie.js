756084dbb63f9864916c43344888574043e47dad
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-21 15:13:53 +0530

    add movie hooks

---
+++ src/hooks/useMovie.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import { extractSeriesIdFromURL } from "../configs/helpers";
+import { axiosPrivateInstance, axiosPublicInstance } from "../configs/axios";
+
+export const useMovieTMDB = (movieId) => {
+    const [movie, setMovie] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        axiosPublicInstance
+            .get(`/api/tmdb/movies/${extractSeriesIdFromURL(movieId)}`)
+            .then(({ data }) => {
+                setMovie(data);
+                setIsLoading(false);
+            })
+            .catch((error) => console.log(error));
+    }, [movieId]);
+
+    return { movie, isLoading };
+};
+
+export const useMovieDetails = (movieId, setIsSending, hasRated) => {
+    const [isDetailsLoading, setIsDetailsLoading] = useState(true);
+    const [movieDetails, setMovieDetails] = useState();
+
+    const auth = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        const axiosInstance = axiosPrivateInstance(auth);
+        axiosInstance
+            .get(`/api/movies/${extractSeriesIdFromURL(movieId)}`)
+            .then(({ data }) => {
+                setIsDetailsLoading(false);
+                setMovieDetails(data[0]);
+                setIsSending(false);
+            })
+            .catch((err) => {
+                setIsDetailsLoading(false);
+            });
+    }, [hasRated]);
+
+    return { movieDetails, isDetailsLoading };
+};
