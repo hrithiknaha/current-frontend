5ce01208594291cb3b9f9367ac5d09636f99af3c
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-25 23:19:03 +0530

    fixed styles of small loader and reverted next card styles

--- src/components/lists/NextEpisodeCard.js
+++ src/components/lists/NextEpisodeCard.js
@@ -12,13 +12,12 @@ const NextEpisodeCard = ({ series, nextEpisode, watchedEpisodes }) => {
             }`}
             className="bg-white overflow-hidden flex-none rounded-lg shadow-md w-20 h-32 lg:w-40 lg:h-64">
             <img
-                style={{ height: "248px" }}
                 className="object-cover"
                 src={`https://image.tmdb.org/t/p/w300/${series.poster_path}`}
                 alt={`${series.title}`}
             />
-            <div className="h-2 w-full bg-gray-300">
-                <div className="h-2 bg-orange-500" style={{ width: `${watchedPercentage}%` }}></div>
+            <div className="h-4 w-full bg-gray-300">
+                <div className="h-4 bg-orange-500" style={{ width: `${watchedPercentage}%` }}></div>
             </div>
         </Link>
     );
