31b7f3745c4b08ca9b066e93b6fb4ff916041961
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-25 23:15:48 +0530

    moved next episode logic to backend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,73 +31,16 @@ function Home() {
     useEffect(() => {
         const axiosInstance = axiosPrivateInstance(auth);
 
-        axiosInstance.get(`/api/users/${auth.username}`).then(({ data }) => {
-            if (data.series.length > 0) {
-                const promises = data.series.map((show) => {
-                    const series_id = show.series_id;
-
-                    let seasonNumber = 1;
-                    let episodeNumber = 0;
-
-                    if (show.episodes.length > 0) {
-                        seasonNumber = show.episodes[show.episodes?.length - 1].season_number;
-                        episodeNumber = show.episodes[show.episodes?.length - 1].episode_number;
-                    }
-
-                    if (show.number_of_episodes === show.episodes.length) return null;
-
-                    return axiosPublicInstance
-                        .get(`/api/tmdb/series/${series_id}`)
-                        .then((response) => {
-                            const seriesDetails = response.data;
-
-                            //total seasons
-                            const totalSeasons = seriesDetails.number_of_seasons;
-
-                            //Next Episode?
-                            const currentSeason = seriesDetails.seasons.filter(
-                                (season) => season.season_number === seasonNumber
-                            )[0];
-
-                            if (seasonNumber <= totalSeasons) {
-                                if (episodeNumber < currentSeason.episode_count) episodeNumber += 1;
-                                else if (episodeNumber === currentSeason.episode_count) {
-                                    episodeNumber = 1;
-                                    seasonNumber += 1;
-                                }
-
-                                return axiosPublicInstance
-                                    .get(
-                                        `/api/tmdb/series/${series_id}/season/${seasonNumber}/episode/${episodeNumber}`
-                                    )
-                                    .then((nextEpisodeData) => {
-                                        const nextEpisode = nextEpisodeData.data;
-                                        return { seriesDetails, nextEpisode, show };
-                                    });
-                            } else {
-                                return null; // Return null for cases where there is no next episode
-                            }
-                        })
-                        .catch((error) => {
-                            console.error(error); // Handle errors for individual Axios requests locally
-                            return null; // Return null for failed requests
-                        });
-                });
-
-                Promise.all(promises.filter(Boolean)) // Filter out null values from the promises array
-                    .then((results) => {
-                        const validResult = results
-                            .filter((r) => r !== null)
-                            .sort((a, b) => (a.show.updatedAt < b.show.updatedAt ? 1 : -1));
-                        setNextDetails(validResult);
-                        setIsSeriesLoading(false);
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                        setIsSeriesLoading(false);
-                    });
-            }
-        });
+        axiosInstance
+            .get(`/api/episodes/${auth.username}/continue/watching`)
+            .then(({ data }) => {
+                setNextDetails(data);
+                setIsSeriesLoading(false);
+            })
+            .catch((err) => {
+                console.log(err);
+                setIsSeriesLoading(false);
+            });
     }, [auth.username]);
 
     const handleSubmit = (e) => {
