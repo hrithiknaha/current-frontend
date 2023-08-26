4b156bd1ec9079bbc218b68c66cf9d4517694222
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-26 18:48:45 +0530

    add custom color on completion

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -241,7 +241,7 @@ const Profile = () => {
                                     {loggedUser.series.filter((s) => s.episodes.length === s.number_of_episodes)
                                         .length > 0 && (
                                         <div>
-                                            <h1 className="inline-block bg-orange-500 text-white px-4 py-2 mb-4 rounded-lg">
+                                            <h1 className="inline-block bg-purple-500 text-white px-4 py-2 mb-4 rounded-lg">
                                                 Completed
                                             </h1>
                                             <SeriesList
