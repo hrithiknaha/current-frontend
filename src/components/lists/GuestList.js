e30a4517198ae8db1d128e48ec865fab2b7de60d
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-26 19:40:14 +0530

    changed height for cast/guest/crew add link to guest

--- src/components/lists/GuestList.js
+++ src/components/lists/GuestList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const defaultImg = "https://t4.ftcdn.net/jpg/02/17/34/67/240_F_217346782_7XpCTt8bLNJqvVAaDZJwvZjm0epQmj6j.jpg";
 
@@ -8,20 +9,21 @@ const GuestList = ({ guests }) => {
             <h3 className="font-bold py-2">Guest Casts</h3>
             <div className="flex overflow-x-auto gap-4">
                 {guests.map((actor) => (
-                    <div
+                    <Link
+                        to={`/person/${actor.id}`}
                         key={actor.id}
-                        className="bg-gray-100 overflow-hidden flex-none rounded-lg shadow w-24 lg:w-36 h-56 lg:h-72">
+                        className="bg-gray-100 overflow-hidden flex-none rounded-lg shadow w-24 lg:w-36">
                         <img
                             src={`https://image.tmdb.org/t/p/w300/${actor.profile_path}`}
                             alt={actor.name}
-                            className="rounded-t w-full h-3/5 lg:h-52 object-cover"
+                            className="rounded-t w-full object-cover"
                             onError={(e) => (e.target.src = defaultImg)}
                         />
                         <div className="p-3">
                             <h3 className="text-sm font-semibold">{actor.name}</h3>
                             <p className="text-xs text-gray-600">{actor.character}</p>
                         </div>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </div>
