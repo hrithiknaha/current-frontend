3f28b15fc64051424bf6cb066d29ed22a025a600
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-26 19:24:25 +0530

    matching rating with status

--- src/components/configs/RatingAndTimeDetails.js
+++ src/components/configs/RatingAndTimeDetails.js
@@ -1,9 +1,20 @@
-const RatingDetails = ({ data, completion }) => {
+const RatingDetails = ({ status, data, completion }) => {
+    let statusClassName;
+
+    if (status === "Canceled") {
+        statusClassName = "bg-red-500";
+    } else if (status === "Ended") {
+        statusClassName = "bg-purple-500";
+    } else {
+        statusClassName = "bg-orange-500";
+    }
+
     return (
-        <div class="bg-orange-500 px-4 py-1 rounded-lg shadow-md text-white flex gap-4 items-center justify-between">
-            <div class="text-3xl font-semibold">{data.rating}</div>
-            <div class="text-sm opacity-80">{data.runtime} min</div>
-            <div class="text-sm font-semibold">{completion}%</div>
+        <div
+            className={`${statusClassName} px-4 py-1 rounded-lg shadow-md text-white flex gap-4 items-center justify-between`}>
+            <div className="text-3xl font-semibold">{data.rating}</div>
+            <div className="text-sm opacity-80">{data.runtime} min</div>
+            <div className="text-sm font-semibold">{completion}%</div>
         </div>
     );
 };
