d7fe9e2dbf31305172b3cec1ea3568abe2a03f9c
Hrithik Naha <naha.hrithkk@gmail.com>
2024-02-13 20:42:51 +0530

    update label

--- src/components/stats/BarChartGraph.js
+++ src/components/stats/BarChartGraph.js
@@ -4,7 +4,7 @@ const BarChartGraph = ({ data, fetchMovies, action }) => {
     return (
         <ResponsiveContainer width="100%" height="100%">
             <BarChart data={data} cursor="pointer">
-                <XAxis dataKey="name" interval={0} style={{ fontSize: "0.7rem" }} />
+                <XAxis dataKey="label" interval={0} style={{ fontSize: "0.7rem" }} />
                 <YAxis width={20} style={{ fontSize: "0.7rem" }} />
                 <Tooltip />
                 <Bar dataKey="count" fill="#f95d6a" onClick={(e) => fetchMovies(e, action)} />
